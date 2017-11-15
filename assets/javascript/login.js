cf23750765863d7d4024e4495a4d2f3b46e7b4f8
Moses <rlakim@ucdavis.edu>
2017-11-14 20:59:13 -0800

    added javascript files and started to working on login page javascript

---
+++ assets/javascript/login.js
@@ -0,0 +1,16 @@
+
+  var config = {
+  	apiKey: "AIzaSyDi_hh8Al-BcWhprUm5Amx0f2wgxIxO_DU",
+  	authDomain: "mydatabaseproj-mosh.firebaseapp.com",
+  	databaseURL: "https://mydatabaseproj-mosh.firebaseio.com",
+  	projectId: "mydatabaseproj-mosh",
+  	storageBucket: "mydatabaseproj-mosh.appspot.com",
+  	messagingSenderId: "812655889128"
+  }
+
+
+
+  firebase.initializeApp(config)
+
+
+  var database = firebase.database()
