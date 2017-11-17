fb70c438da668dbe8dfbec941bd7f1dbed7ae167
Moses <rlakim@ucdavis.edu>
2017-11-16 20:40:30 -0800

    very simple signup and login function with javascript and firebase auth, database

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,22 +1,47 @@
 
-  var config = {
-  	apiKey: "AIzaSyDi_hh8Al-BcWhprUm5Amx0f2wgxIxO_DU",
-  	authDomain: "mydatabaseproj-mosh.firebaseapp.com",
-  	databaseURL: "https://mydatabaseproj-mosh.firebaseio.com",
-  	projectId: "mydatabaseproj-mosh",
-  	storageBucket: "mydatabaseproj-mosh.appspot.com",
-  	messagingSenderId: "812655889128"
-  }
 
-
-
-  firebase.initializeApp(config)
-
-
-  var database = firebase.database()
-
-
-
+// toggle between login and signup
 $('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+  $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+
+// login in button clicked!
+$('#loginButton').on('click', function(event){
+
+  event.preventDefault()
+  //get the email value from textfield
+  var email = $('#emailForLogin').val()
+
+  //get the password value from textfield
+  var password = $('#passwordForLogin').val()
+
+
+  console.log(email)
+  console.log(password)
+
+  firebase.auth().signInWithEmailAndPassword(email, password).then(function(user){
+
+    document.location.href = "Profile.html"
+
+    if(user){
+      console.log("success : " + user.val().name)
+    } else {
+      console.log("no such user")
+    }
+  }).catch(function(error) {
+   // Handle errors
+  })
+
+})
+
+
+
+
+
+/*// Sign out user
+firebase.auth().signOut()
+ .catch(function (error) {
+   // Handle errors
+ });
+*/
---
+++ assets/javascript/signup.js
@@ -0,0 +1,76 @@
+
+
+var userName
+var email
+var password
+
+//sign up button clicked!
+$('#createButton').on('click', function(){
+
+
+	// get the user_name
+	userName = $('#userNameForSignup').val().trim()
+
+	// get the email
+	email = $('#emailAddressForSignup').val().trim()
+
+	// get the password
+	password = $('#passwordForSignup').val().trim()
+
+
+	console.log(userName)
+	console.log(email)
+	console.log(password)
+
+
+	if(email && password){
+
+		firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
+			firebase.auth().onAuthStateChanged(function(user){
+
+				if(user) {
+					database.ref('/user/'+user.uid).set({
+						name: userName,
+						email: email,
+						password: password
+					})
+
+					document.location.href = "Profile.html"
+
+				} else {
+					console.log("can not grab the user")
+				}
+
+			})
+
+
+
+		}).catch(function(error) {
+
+			var errorCode = error.code
+			var errorMessage = error.message
+
+			console.log(errorCode + " " + errorMessage)
+
+		})
+
+
+	} else {
+		$('#userNameForSignup').val() = " "
+		$('#emailAddressForSignup').val() = " "
+		$('#passwordForSignup').val() = " "
+
+	}
+	
+
+
+})
+
+
+
+
+
+
+
+
+
